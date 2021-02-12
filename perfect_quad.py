94a81e7b4e3cbfe8f5bfd5b2a3ba483c0fe3b86f
Panagiotis <panagiotis.tsakas2@gmail.com>
2021-02-12 19:00:42 +0200

    perfect quad tree and quick sketch from graphviz

---
+++ perfect_quad.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+
+# set directory at file location
+abspath = os.path.abspath(__file__)
+dirname = os.path.dirname(abspath)
+os.chdir(dirname)
+
+# go to Quad_Tree folder directory
+os.chdir('./Quad_Tree')
+sys.path.insert(1, os.getcwd())
+from QuadTree import QuadTree
+
+# set directory at file location again
+abspath = os.path.abspath(__file__)
+dirname = os.path.dirname(abspath)
+os.chdir(dirname)
+
+lista = [
+    (1,4), (2,4), (3,4), (4.5,4.5),
+    (1,3), (2,3), (3,3), (4,3),
+    (1,2), (2,2), (3,2), (4,2),
+    (0.5,0.5), (2,1), (3,1), (4,1)
+]
+
+capacity = 1
+qtree = QuadTree(capacity, lista)
+qtree.graph()
+
+# plt.scatter(*zip(*lista))
+# plt.show()
