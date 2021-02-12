9d5a645cd078e426c3fbfcb225ee9b19e7f087b7
Panagiotis <panagiotis.tsakas2@gmail.com>
2021-02-12 19:00:53 +0200

    draw target points

---
+++ target_points.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+
+inner_targets = [   (250,750), (750,750),
+                    (250,250), (750,250)
+                ]
+
+outter_targets = [                  (500, 3000),
+                    (-2000, 500),                 (3000, 500),
+                                    (500, -2000)
+                ]
+
+# inner_targets = [   (166.6, 833.3), (500, 833.3), (833.3, 833.3),
+#                     (166.6, 500),   (500, 500),   (833.3, 500),
+#                     (166.6, 166,6), (500, 166.6), (833.3, 166.6)
+#                 ]
+
+# outter_targets = [  (-2000, 3000), (500, 3000), (3000, 3000),
+#                     (-2000, 500),               (3000, 500),
+#                     (-2000, -2000),(500, -2000),(3000, -2000)
+#                 ]
+
+fig,ax = plt.subplots()
+
+plt.scatter(*zip(*inner_targets), label='inner targets')
+plt.scatter(*zip(*outter_targets), label='outter targets')
+ax.legend(bbox_to_anchor=(0.67, 0.85))
+
+currentAxis = plt.gca()
+currentAxis.add_patch(Rectangle((0, 0), 1000, 1000, alpha=1, fill=False))
+
+plt.show()
